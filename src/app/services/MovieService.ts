cfc94d6491511a05d9aa71856193e3c167908ca3
PEmbarach <pabloembarach@gmail.com>
2026-05-05 11:48:40 +0000

    feat: create MovieService with all TMDB API methods

---
+++ src/app/services/MovieService.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MovieService {
+  constructor (private httpClient:HttpClient){}
+
+  getTrendingMovies(){
+    return this.httpClient.get(`${environment.apiBaseUrl}/trending/movie/day?api_key=${environment.apiKey}`);
+  }
+
+  searchMovies(query:string){
+    return this.httpClient.get(`${environment.apiBaseUrl}/search/movie?query=${query}&api_key=${environment.apiKey}`)
+  }
+
+  getMovieCredits(id:number){
+    return this.httpClient.get(`${environment.apiBaseUrl}/movie/${id}/credits?api_key=${environment.apiKey}`)
+  }
+
+  getPersonDetails(personId:number){
+    return this.httpClient.get(`${environment.apiBaseUrl}/person/${personId}?api_key=${environment.apiKey}`)
+  }
+
+  getPersonMovieCredits(personId:number){
+    return this.httpClient.get(`${environment.apiBaseUrl}/person/${personId}/movie_credits?api_key=${environment.apiKey}`)
+  }
+}
