546759f28dee2e944014d49ec1a288a088740b98
PEmbarach <pabloembarach@gmail.com>
2026-05-05 12:47:10 +0000

    fix: remove environment files from tracking

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: '48991c0c3078d536540c572868fa4986',
-  apiBaseUrl: 'https://api.themoviedb.org/3',
-  apiImageUrl: 'https://image.tmdb.org/t/p/w500'
-};
