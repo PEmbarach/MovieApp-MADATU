e8414c64fece11898b3313cd8b19e752b015ae99
PEmbarach <pabloembarach@gmail.com>
2026-05-06 18:07:34 +0000

    feat: implement MovieDetailsPage with credits fetch

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { MovieService } from '../services/MovieService';
 
 @Component({
   selector: 'app-movie-details',
@@ -11,10 +14,20 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class MovieDetailsPage implements OnInit {
-
-  constructor() { }
+  movie:any = [];
+  cast:any[] = [];
+  crew:any[] = [];
+  imageBaseUrl: string = environment.apiImageUrl;
+  
+  constructor(private movieService: MovieService, private router:Router) {
+    this.movie = this.router.getCurrentNavigation()?.extras.state?.['movie'];
+   }
 
   ngOnInit() {
+    this.movieService.getMovieCredits(this.movie.id).subscribe((data:any) =>{
+      this.cast = data.cast;
+      this.crew = data.crew;
+    })
   }
 
 }
