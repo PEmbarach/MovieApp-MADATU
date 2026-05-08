0f10df27ffd310c0c2c4e9c75300787fd8ecfd8c
PEmbarach <pabloembarach@gmail.com>
2026-05-08 09:29:20 +0000

    feat: create FavouritesService with add, remove, get and isFavourite methods

---
+++ src/app/services/FavoritesService.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FavoritesService {
+  constructor(private storage: Storage) {
+    this.init();
+  }
+
+  async init() {
+    await this.storage.create();
+  }
+
+  async addFavourites(movie: any) {
+    const favourites = await this.getFavourites();
+    favourites.push(movie);
+    await this.storage.set('favourites', favourites);
+  }
+
+  async removeFavoutire(movieID: number) {
+    const favourites = await this.getFavourites();
+    const update = favourites.filter((m: any) => m.id !== movieID);
+    await this.storage.set('favourites', update);
+  }
+
+  async getFavourites() {
+    return await this.storage.get('favourites') || [];
+  }
+
+  isFavourite(movieID: number) { 
+    const favourites = await this.getFavourites();
+    const update = favourites.filter((m: any) => m.id !== movieID);
+    await this.storage.set('favourites', update);
+  }
+
+}
