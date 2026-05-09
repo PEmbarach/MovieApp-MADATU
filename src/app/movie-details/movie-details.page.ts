e206683528132c9dd7bbcf79a9710b3e74520827
PEmbarach <pabloembarach@gmail.com>
2026-05-09 11:12:01 +0000

    feat: implement Add/Remove Favourites button on Movie Details page

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -8,6 +8,7 @@ import { addIcons,  } from 'ionicons';
 import { heart, homeOutline } from 'ionicons/icons';
 import { DataService } from '../services/DataService';
 import { ViewWillEnter } from '@ionic/angular';
+import { FavoritesService } from '../services/FavoritesService';
 import {
   IonCard,
   IonCardContent,
@@ -29,6 +30,7 @@ import {
   IonItem
 } from '@ionic/angular/standalone'
 
+
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
@@ -46,18 +48,24 @@ export class MovieDetailsPage implements ViewWillEnter {
   movie:any = [];
   cast:any[] = [];
   crew:any[] = [];
-  imageBaseUrl: string = environment.apiImageUrl;
+  imageBaseUrl:string = environment.apiImageUrl;
+  isFavourite:boolean = false;
   
-  constructor(private movieService: MovieService, private router:Router, private dataService:DataService) {
+  constructor(private movieService: MovieService, 
+              private router:Router, 
+              private dataService:DataService, 
+              private favouriteService:FavoritesService
+              ) {
     addIcons({heart, homeOutline})
   }
 
-  ionViewWillEnter() {
+  async ionViewWillEnter() {
     this.movie = this.dataService.selectedMovie;
     this.movieService.getMovieCredits(this.movie.id).subscribe((data:any) => {
       this.cast = data.cast;
       this.crew = data.crew;
     });
+    this.isFavourite = await this.favouriteService.isFavourite(this.movie.id);
   }
 
   goToPersonDetails(person:any){
@@ -71,5 +79,15 @@ export class MovieDetailsPage implements ViewWillEnter {
   
   goToHomePage(){
     this.router.navigate(['/home']);
+  }
+
+  async addToFavourites(){
+    await this.favouriteService.addFavourites(this.movie);
+    this.isFavourite = true;
+  }
+
+  async removeFromFavourites(){
+    await this.favouriteService.removeFavoutire(this.movie.id);
+    this.isFavourite = false;
   }
 }
